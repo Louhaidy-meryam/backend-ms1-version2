975d3b61df859b007e25eb839c559d095ac47940
meryamlouhaidy <79732423+meryamlouhaidy@users.noreply.github.com>
2024-04-08 22:53:10 +0200

    version2

---
+++ src/test/java/testapplication.java
@@ -0,0 +1,13 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class testapplication {
+
+    @Test
+    void itShouldPrintHello() {
+        System.out.println("hello");
+        Assertions.assertThat(true).isEqualTo(true);
+
+    }
+}
+//first
